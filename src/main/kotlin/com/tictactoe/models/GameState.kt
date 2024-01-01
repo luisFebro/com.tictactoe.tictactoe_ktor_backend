241554bcd52051d105fc1473ae11d13fb7dca574
luisFebro <mr.febro@gmail.com>
2024-01-01 06:09:48 -0300

    Initial commit

---
+++ src/main/kotlin/com/tictactoe/models/GameState.kt
@@ -0,0 +1,46 @@
+package com.tictactoe.models
+
+import kotlinx.serialization.Serializable
+
+// exp: https://youtu.be/sKCCwl5lNBk?t=729s
+@Serializable
+data class GameState(
+    val playerAtTurn: Char? = 'X',
+    val field: Array<Array<Char?>> = emptyField(),
+    val winningPlayer: Char? = null,
+    val isBoardFull: Boolean = false,
+    val connectedPlayers: List<Char> = emptyList()
+) {
+    companion object {
+        fun emptyField(): Array<Array<Char?>> {
+            return arrayOf(
+                arrayOf(null, null, null),
+                arrayOf(null, null, null),
+                arrayOf(null, null, null),
+            )
+        }
+    }
+
+    // exp: https://youtu.be/sKCCwl5lNBk?t=915s
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GameState
+
+        if (playerAtTurn != other.playerAtTurn) return false
+        if (!field.contentDeepEquals(other.field)) return false
+        if (winningPlayer != other.winningPlayer) return false
+        if (isBoardFull != other.isBoardFull) return false
+        return connectedPlayers == other.connectedPlayers
+    }
+
+    override fun hashCode(): Int {
+        var result = playerAtTurn?.hashCode() ?: 0
+        result = 31 * result + field.contentDeepHashCode()
+        result = 31 * result + (winningPlayer?.hashCode() ?: 0)
+        result = 31 * result + isBoardFull.hashCode()
+        result = 31 * result + connectedPlayers.hashCode()
+        return result
+    }
+}
---
+++ src/main/kotlin/com/tictactoe/models/MakeTurn.kt
@@ -0,0 +1,3 @@
+package com.tictactoe.models
+
+data class MakeTurn()
